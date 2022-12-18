06fb61ba1d796bb142566dd2729fe563c3f31b0b
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-18 20:05:17 +0300

    usecallback örneği tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-
-
+import React from "react";
+import Todos from "./Todos";
 
 function App() {
+  const [sayac, sayacGuncelle] = React.useState(0);
+  const [yapilacaklar, yapilacaklarGuncelle] = React.useState([]);
+
+  const arttir = () => {
+    sayacGuncelle((oncekiDeger) => oncekiDeger + 1);
+  };
+
+  const isEkle = React.useCallback(()=>{
+  
+      yapilacaklarGuncelle((oncekiListe) => [...oncekiListe, "Yeni yapılacak iş"]);
+    },[])
+  
+
   return (
-    <div >
-      Merhaba..
-    </div>
+    <>
+      <Todos yapilacaklar={yapilacaklar} isEkle={isEkle} />
+
+      <hr />
+
+      <div>
+        Sayaç: {sayac}
+        <button onClick={arttir}>+</button>
+      </div>
+    </>
   );
 }
 
