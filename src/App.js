ea401631136c55ed0d4c7ac82762a3f39258bf1f
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-18 17:11:10 +0300

    useref örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
-
+import React from "react";
 
 function App() {
+let eleman1=React.useRef()
+
+React.useEffect(()=>{
+
+  setTimeout(()=>{
+    eleman1.current.textContent = "ikinci yazı"
+
+  },3000)
+  
+})
+
+
   return (
     <div >
-      Merhaba..
+    <p ref={eleman1}>İlk yazı</p>
     </div>
   );
 }
