e19f0a582e6e687f30698f712ff1f373f9029b62
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-18 18:00:28 +0300

    memo örneği yapıldı.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-
+  import C1 from "./C1";
+  import React from "react";
 
 
 function App() {
+const[yazi,yaziGuncelle]=React.useState("")
+
+
+
   return (
     <div >
       Merhaba..
+      <input type="text" value={yazi} onChange={(olay)=>{yaziGuncelle(olay.target.value)}} />
+      <C1/>
     </div>
   );
 }
