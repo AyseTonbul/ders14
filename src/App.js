52fa78fbc5e87bfea79c6d21de839718aa500358
Ayse Tonbul <aysetonbul77@hotmail.com>
2022-12-18 18:45:35 +0300

    usememo ornek tamamlandı.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-
-
+import React from "react";
 
 function App() {
+  const [degisken1, degisken1Guncelle] = React.useState(1)
+  const [degisken2, degisken2Guncelle] = React.useState(5)
+  const [degisken3, degisken3Guncelle] = React.useState(1)
+
+  const ozelDeger = React.useMemo( ()=>{return uzunSurenIslem(degisken2)}, [degisken2]   )
+
+  function uzunSurenIslem(sayi) {
+    let cikti = sayi * sayi
+    let zaman = new Date().getSeconds()
+    return cikti + "-" + zaman
+  }
+
+  function besArttir() {
+    degisken3Guncelle( (oncekiDeger)=>oncekiDeger+5 )
+  }
+
   return (
-    <div >
-      Merhaba..
+    <div className="App">
+      Merhaba!
+      <hr />
+      Değişken 3: {degisken3}
+      <hr />
+      Özel değer: {ozelDeger}
+      <hr />
+      <button onClick={besArttir}>Değişken 3'ü 5 arttır</button>
+
     </div>
   );
 }
-
 export default App;
